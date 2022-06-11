156c28ebb43989c157d73e5282c088d17a4e8f42
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-10 19:38:17 -0500

    ej04: piezas alineadas, falta unir ambas partes

--- ej04.c
+++ ej04.c
@@ -3,6 +3,7 @@
 
 void display(){
   char** cuad = repeatH(join(reverse(whiteSquare),whiteSquare),4);
-  char** piezas = join(rook, knight);
+  char** piezas = join(join(join(rook, knight), bishop), queen);
+  piezas = join(join(join(join(piezas, king), bishop), knight), rook);
   interpreter(piezas);
 }

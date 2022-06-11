bbabb1bb7bd91e5aa7da3968507442bde6c8ad69
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-10 19:41:41 -0500

    ej04: partes unidas, ejercicio terminado

--- ej04.c
+++ ej04.c
@@ -5,5 +5,6 @@ void display(){
   char** cuad = repeatH(join(reverse(whiteSquare),whiteSquare),4);
   char** piezas = join(join(join(rook, knight), bishop), queen);
   piezas = join(join(join(join(piezas, king), bishop), knight), rook);
+  piezas = superImpose(piezas, cuad);
   interpreter(piezas);
 }

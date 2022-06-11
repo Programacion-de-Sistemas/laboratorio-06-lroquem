a9ba521fb3e515ac0bc3d39056400148d10af2aa
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-10 20:10:36 -0500

    ej06: se crearon variables para almacenar tanto piezas como peones blancos y negros, se unio todo al final, ejercicio terminado

--- ej06.c
+++ ej06.c
@@ -5,6 +5,13 @@ void display(){
   char** cuad = repeatH(join(reverse(whiteSquare),whiteSquare),4);
   char** piezas = join(join(join(rook, knight), bishop), queen);
   piezas = join(join(join(join(piezas, king), bishop), knight), rook);
-  piezas = superImpose(piezas, cuad);
-  interpreter(piezas);
+  char** piezasNegras = reverse(piezas);
+  char** peones = repeatH(pawn,8);
+  char** peonesNegros = reverse(peones);
+  char** final = superImpose(piezas, cuad);
+  final = up(final, superImpose(peones,reverse(cuad)));
+  final = up(final, up(cuad, reverse(cuad)));
+  final = up(final, superImpose(peonesNegros, cuad));
+  final = up(final, superImpose(piezasNegras, reverse(cuad)));
+  interpreter(final);
 }

02ed1c27da45f75e443ae70a455f8f8ff4bc2854
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-10 19:53:24 -0500

    ej06: empezamos con el ej04 como base

---
+++ ej06.c
@@ -0,0 +1,10 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** cuad = repeatH(join(reverse(whiteSquare),whiteSquare),4);
+  char** piezas = join(join(join(rook, knight), bishop), queen);
+  piezas = join(join(join(join(piezas, king), bishop), knight), rook);
+  piezas = superImpose(piezas, cuad);
+  interpreter(piezas);
+}

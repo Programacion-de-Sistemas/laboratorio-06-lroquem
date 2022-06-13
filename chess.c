a29015e35081df3c639388a9931da620c1d0bbe2
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-13 10:57:13 -0500

    chess.c: implementando flipV, recorriendo array

---
+++ chess.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "chess.h"
+
+char** flipV(char** fig){
+  char** = malloc(sizeof(fig));
+  int j = 0;
+  while(fig[i]){
+    int i = 0;
+    while(fig[j][i]){
+      printf("%c ",fig[j][i]);
+      i++;
+    }
+    printf("\n");
+    j++;
+  }
+}
